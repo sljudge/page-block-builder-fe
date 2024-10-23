72a11620af40da33e0ad8cf06b9266ae4e061f3f
Sam <sam.judge@huguenots.co.uk>
2024-10-23 17:01:21 +0100

    utils

---
+++ src/utils/Console.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+class Console {
+  log(...args: any[]): void {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(...args);
+    }
+  }
+
+  info(...args: any[]): void {
+    if (process.env.NODE_ENV !== 'production') {
+      console.info(...args);
+    }
+  }
+
+  warn(...args: any[]): void {
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(...args);
+    }
+  }
+
+  error(...args: any[]): void {
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(...args);
+    }
+  }
+
+  debug(...args: any[]): void {
+    if (process.env.NODE_ENV !== 'production') {
+      console.debug(...args);
+    }
+  }
+}
+
+export default new Console();
