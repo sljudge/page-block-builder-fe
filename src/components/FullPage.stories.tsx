388cf22fd51d5d839f965299e65c5215b15e20cf
Sam <sam.judge@huguenots.co.uk>
2024-10-24 17:14:04 +0100

    add full page story

---
+++ src/components/FullPage.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta } from '@storybook/react';
+
+// import Nav from './Nav';
+import Hero, { HeroProps, HeroStory } from './Hero';
+import { NavStory } from './Nav';
+
+export default {
+  title: 'Components/FullPage',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen'
+  }
+} satisfies Meta;
+
+export const Default = () => {
+  return (
+    <div>
+      <NavStory withMockSections={false} />
+      <Hero {...(HeroStory.args as HeroProps)} />
+    </div>
+  );
+};
