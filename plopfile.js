5eb17add664b4ab5e006d485448abd40f310a569
Sam <sam.judge@huguenots.co.uk>
2024-10-25 18:43:40 +0100

    add block template

--- plopfile.js
+++ plopfile.js
@@ -1,29 +1,56 @@
 module.exports = (plop) => {
-    plop.setGenerator("component", {
-        description: "Create a component",
-        prompts: [
-            {
-                type: "input",
-                name: "name",
-                message: "What is your component name?",
-            },
-        ],
-        actions: [
-            {
-                type: "add",
-                path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-                templateFile: "templates/Component/Component.js.hbs",
-            },
-            {
-                type: "add",
-                path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
-                templateFile: "templates/Component/ComponentStory.js.hbs",
-            },
-            {
-                type: "add",
-                path: "src/components/{{pascalCase name}}/index.ts",
-                templateFile: "templates/Component/index.js.hbs",
-            },
-        ],
-    });
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/Component/Component.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/Component/ComponentStory.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/Component/index.js.hbs'
+      }
+    ]
+  });
+  plop.setGenerator('block', {
+    description: 'Create a page section block',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your block name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/blocks/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/Block/Block.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/blocks/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/Block/Blockstory.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/blocks/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/Block/index.js.hbs'
+      }
+    ]
+  });
 };
