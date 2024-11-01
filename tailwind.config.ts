c266d26b90c626dc60ad48fd9913da265a5393b0
Sam <sam.judge@huguenots.co.uk>
2024-11-01 11:13:20 +0000

    add layout files to tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -123,7 +123,8 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/blocks/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/blocks/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layout/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     screens: Object.fromEntries(
