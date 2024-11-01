1f797276e0dbb74438bfef5251a14ab456699bdc
Sam <sam.judge@huguenots.co.uk>
2024-11-01 15:45:01 +0000

    add header to testimonials block

--- src/blocks/Testimonials/Testimonials.tsx
+++ src/blocks/Testimonials/Testimonials.tsx
@@ -4,11 +4,17 @@ import { BackgroundColor } from '@/types';
 
 import TestimonialsComponent from '@/components/Testimonials';
 
-export const Testimonials = ({ background_color, num_cols, items }: TestimonialsBlock['item']) => {
+export const Testimonials = ({
+  background_color,
+  num_cols,
+  items,
+  title
+}: TestimonialsBlock['item']) => {
   return (
     <BlockContainer
       colorScheme={(background_color?.key ?? BackgroundColor.primary) as BackgroundColor}
     >
+      {title && <h2 className="text-center">{title}</h2>}
       <TestimonialsComponent numCols={num_cols} items={items} />
     </BlockContainer>
   );
