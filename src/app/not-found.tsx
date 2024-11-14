438d2912a85a92919d3645caed6896a8a88c6927
Sam <sam.judge@huguenots.co.uk>
2024-11-14 16:12:55 +0000

    force 404 page dynamic

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default function NotFound() {
   return (
     <div className="h-screen w-screen bg-secondary p-xxl">
