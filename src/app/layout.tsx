007628071d25389e0ca42c6e31efb5e7b70136eb
Sam Judge <samuel.judge.92@gmail.com>
2025-01-14 12:11:16 +0000

    fix html nesting for footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,26 +27,28 @@ export default async function RootLayout({
       <head>
         <link rel="icon" href={companyInfo.logo} sizes="any" />
       </head>
-      <body className="bg-primary antialiased">{children}</body>
-      <footer className="-mt-xxl bg-invert py-xs text-invert">
-        <div className="container mx-auto flex justify-end">
-          <div className="flex items-center gap-x-xs">
-            <span
-              data-testid="icon"
-              className="material-icons material-symbols-outlined text-lg max-w-full"
-            >
-              policy
-            </span>
-            <a
-              href={companyInfo.privacy_notice}
-              target="_blank"
-              className="text-lg text-right underline"
-            >
-              Privacy notice
-            </a>
+      <body className="bg-primary antialiased">
+        {children}
+        <footer className="-mt-xxl bg-invert py-xs text-invert">
+          <div className="container mx-auto flex justify-end">
+            <div className="flex items-center gap-x-xs">
+              <span
+                data-testid="icon"
+                className="material-icons material-symbols-outlined text-lg max-w-full"
+              >
+                policy
+              </span>
+              <a
+                href={companyInfo.privacy_notice}
+                target="_blank"
+                className="text-lg text-right underline"
+              >
+                Privacy notice
+              </a>
+            </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </body>
     </html>
   );
 }
